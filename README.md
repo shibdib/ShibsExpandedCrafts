a843c2366e4299a668709e689a3bf4450f55d221
Bob S <shibdib@users.noreply.github.com>
2024-12-11 19:35:02 -0500

    Add AFAK/IFAK re-craft

--- README.md
+++ README.md
@@ -2,9 +2,11 @@ Current Additions
 
 Workshop
 - CMS + Med Tools = New CMS (LVL1)
+- IFAK + AI-2 + Army Bandage + Esmarch = New IFAK (LVL2)
+- AFAK + Car + Army Bandage + CAT-T = New AFAK (LVL2)
 
 Nutrition Unit
 - 2x Large Beef Stew + Can of Salt = Tarker Jerky (LVL2)
 
 Planned Additions
-- You tell me, either make an issue here with ideas/hit me up on discord/comment
+- You tell me, hit me up on discord/comment
--- src/crafts.ts
+++ src/crafts.ts
@@ -35,6 +35,104 @@ export const AddedCrafts = [
         "isEncoded": false,
         "isCodeProduction": false
     },
+    // IFAK + Cheese + Esmarch + Army Bandage = IFAK (LVL2)
+    {
+        "_id": "675a2f1abb84a45d3ec8be78",
+        "areaType": 7,
+        "requirements": [
+            {
+                "templateId": "5e831507ea0a7c419c2f9bd9",
+                "count": 1,
+                "isFunctional": false,
+                "isEncoded": false,
+                "type": "Item"
+            },
+            {
+                "templateId": "5751a25924597722c463c472",
+                "count": 1,
+                "isFunctional": false,
+                "isEncoded": false,
+                "type": "Item"
+            },
+            {
+                "templateId": "5755356824597772cb798962",
+                "count": 1,
+                "isFunctional": false,
+                "isEncoded": false,
+                "type": "Item"
+            },
+            {
+                "templateId": "590c678286f77426c9660122",
+                "count": 1,
+                "isFunctional": false,
+                "isEncoded": false,
+                "type": "Item"
+            },
+            {
+                "areaType": 7,
+                "requiredLevel": 2,
+                "type": "Area"
+            }
+        ],
+        "productionTime": 1800,
+        "needFuelForAllProductionTime": false,
+        "locked": false,
+        "endProduct": "590c678286f77426c9660122",
+        "continuous": false,
+        "count": 2,
+        "productionLimitCount": 0,
+        "isEncoded": false,
+        "isCodeProduction": false
+    },
+    // AFAK + Car + CAT-T + Army Bandage = AFAK (LVL2)
+    {
+        "_id": "675a2f32b33253355980a79f",
+        "areaType": 7,
+        "requirements": [
+            {
+                "templateId": "5751a25924597722c463c472",
+                "count": 1,
+                "isFunctional": false,
+                "isEncoded": false,
+                "type": "Item"
+            },
+            {
+                "templateId": "60098af40accd37ef2175f27",
+                "count": 1,
+                "isFunctional": false,
+                "isEncoded": false,
+                "type": "Item"
+            },
+            {
+                "templateId": "590c661e86f7741e566b646a",
+                "count": 1,
+                "isFunctional": false,
+                "isEncoded": false,
+                "type": "Item"
+            },
+            {
+                "templateId": "60098ad7c2240c0fe85c570a",
+                "count": 1,
+                "isFunctional": false,
+                "isEncoded": false,
+                "type": "Item"
+            },
+            {
+                "areaType": 7,
+                "requiredLevel": 2,
+                "type": "Area"
+            }
+        ],
+        "productionTime": 2700,
+        "needFuelForAllProductionTime": false,
+        "locked": false,
+        "endProduct": "60098ad7c2240c0fe85c570a",
+        "continuous": false,
+        "count": 2,
+        "productionLimitCount": 0,
+        "isEncoded": false,
+        "isCodeProduction": false
+    },
 
     // Nutrition Unit
     // 2x Large Tushonka + Salt = 2x Tarker (Lvl2)
